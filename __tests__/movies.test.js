354d09aa8c6310a5f51b3c2923416aad2dcf6f88
Alejae1998 <alejagr1998@gmail.com>
2022-08-04 12:15:45 -0700

    after triying multiple tables, getting errors and googling my errors test 1 is passing

---
+++ __tests__/movies.test.js
@@ -0,0 +1,50 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('movie routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('/movies should return a list of movies', async () => {
+    const resp = await request(app).get('/movies');
+    expect(resp.status).toEqual(200);
+    expect(resp.body).toEqual([
+      {
+        id: '1',
+        name: 'Blade Runner',
+        director: 'Ridley Scott',
+        genre: 'Science Fiction',
+      },
+      {
+        id: '2',
+        name: 'Fight Club',
+        director: 'David Fincher',
+        genre: 'psychological action drama',
+      },
+      {
+        id: '3',
+        name: 'Remember the Titans',
+        director: 'Boaz Yakin',
+        genre: 'Sports',
+      },
+    ]);
+  });
+
+  it.skip('/movies/:id should return the movie detail', async () => {
+    const resp = await request(app).get('/movies/1');
+    expect(resp.status).toEqual(200);
+    expect(resp.body).toEqual({
+      id: '1',
+      name: 'Blade Runner',
+      director: 'Ridley Scott',
+      genre: 'Science Fiction',
+    });
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,19 +1,16 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE if exists gods;
+DROP TABLE if exists movies;
 
-CREATE TABLE gods (
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  name VARCHAR NOT NULL,
-  known INT NOT NULL,
-  drink VARCHAR NOT NULL
+CREATE TABLE movies (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    director VARCHAR NOT NULL,
+    genre VARCHAR NOT NULL
 );
-INSERT INTO gods (
-  name,
-  known,
-  drink
-) 
-VALUES
-('Zeus', 'Lord of Justice', 'Iced American (no sugar)'), ('Poseidon', 'Known as Neptune', 'Espresso shot'),
-('Athena', 'Goddess of wisdom, war and the crafts', 'Hot Oatmilk Latte'),
-('Apollo', 'Most Loved God', 'Iced Mocha');
+
+INSERT INTO movies (name, director, genre) VALUES
+('Blade Runner', 'Ridley Scott', 'Science Fiction'),
+('Fight Club', 'David Fincher', 'psychological action drama'),
+('Remember the Titans', 'Boaz Yakin', 'Sports');
+
