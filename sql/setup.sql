7f5b2f77e4af1a88743c021a66d412a1e5d10154
Alejae1998 <alejagr1998@gmail.com>
2022-08-05 11:25:43 -0700

    creating cities table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,7 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE if exists movies;
-
+DROP TABLE if exists cities;
 CREATE TABLE movies (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR NOT NULL,
@@ -13,4 +13,16 @@ INSERT INTO movies (name, director, genre) VALUES
 ('Blade Runner', 'Ridley Scott', 'Science Fiction'),
 ('Fight Club', 'David Fincher', 'psychological action drama'),
 ('Remember the Titans', 'Boaz Yakin', 'Sports');
+
+CREATE TABLE cities (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    population INT NOT NULL,
+    motto VARCHAR NOT NULL
+);
+
+INSERT INTO cities (name, population, motto) VALUES
+('Portland', 650380, 'keep portland weird'),
+('New York', 8000000, 'excelsior'),
+('Seattle', 741251, 'city of goodwill');
 
